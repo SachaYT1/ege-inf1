b18215235302b3f114b635ad4ecdd0ca7a3fb9e0
Alexander <sacha2005@bk.ru>
2022-02-02 19:41:09 +0300

    add HomeFragment

--- app/src/main/java/com/example/egeinfnav/MainActivity.java
+++ app/src/main/java/com/example/egeinfnav/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.egeinfnav;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 import android.view.Gravity;
@@ -10,11 +12,12 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
 
         findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
@@ -23,5 +26,15 @@ public class MainActivity extends AppCompatActivity {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+
+//        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        findViewById(R.id.btn_task1).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                navController.navigate(R.id.task_1);
+//            }
+//        });
     }
+
+
 }
