537df0440f5bd4e980c7e7eab9aa1a04a2be874b
Alexander <sacha2005@bk.ru>
2022-05-11 14:48:35 +0300

    доделал 10 фрагмент

--- app/src/main/java/com/example/egeinfnav/fragment/Task10Fragment.java
+++ app/src/main/java/com/example/egeinfnav/fragment/Task10Fragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.egeinfnav.R;
 
@@ -61,6 +64,18 @@ public class Task10Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task10, container, false);
+        String[] array_of_paragraphs = getResources().getStringArray(R.array.TheoryFragment10);
+        View view = inflater.inflate(R.layout.fragment_task10, container, false);
+
+        //условие задачи 1
+        TextView paragraph1 = (TextView)view.findViewById(R.id.paragraph1_fragment10);
+        paragraph1.setText(Html.fromHtml(array_of_paragraphs[0]));
+
+        //решение задачи 1
+        TextView paragraph2 = (TextView)view.findViewById(R.id.paragraph2_fragment10);
+        paragraph2.setText(Html.fromHtml(array_of_paragraphs[1]));
+        ImageView imageView2 = view.findViewById(R.id.image2_fragment10);
+        imageView2.setImageResource(R.drawable.task1_poisk_ty_fragment10);
+        return view;
     }
 }
